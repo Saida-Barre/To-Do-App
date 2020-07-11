40ee1a0ba3fb2fbe3d6da60e0c61e820609c8063
unknown <sbarre33@gmail.com>
2020-07-10 17:19:07 -0700

    wired up our forms and we got our data

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,9 @@
-{
-    console.log("script starting!") //see script starting
-   
-    const input = document.querySelector("input");              //<input type="txt"
-    const addBtn = document.querySelector("button");           //button=addLi()
-    const list = document.querySelector("ul");                //<ul id="myAList"</ul>
-    const error = document.querySelector("#error-output");   //<p id="error-output"></P>
-    
-    let itemArray = []; //Running list of all our items.
-}
+const form=document.querySelector("form");
+console.log(form)
+form.addEventListener("submit", submit);
+function submit (event) { 
+    event.preventDefault();
+    let formData = new FormData(form)
+    console.log(formData.get("newTask"));
 
-//now,create a new item
-function addLi ()
-{
-    var txtVal = document.getElementById('txtVal').value;
-    var listNode = document.getElementById('myAList');
-    var liNode = document.createElement('li');
-    var txtNode = document.createTextNode(txtVal);
-
-    liNode.appendChild(txtNode);
-    listNode.appendChild(liNode);
 }
